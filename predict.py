ca3670eae97e45bd11e70f2b55bc1ca3ec0c80f9
Gabriel Galperin <galperin@alunos.utfpr.edu.br>
2023-12-19 22:56:39 -0300

    add predict file

---
+++ predict.py
@@ -0,0 +1,31 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+from  pandas import DataFrame
+
+file = 'model.bin'
+
+with open(file, 'rb') as f_in:
+   dv, model = pickle.load(f_in)
+
+app = Flask('poisonous')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    parameters = request.get_json() # return a dict
+
+    X = DataFrame.from_dict([parameters])
+    y_pred = model.predict(X)
+
+    result = {
+        "payload": parameters,
+        "poisonous": bool(y_pred)
+    }
+
+    return jsonify(result)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
